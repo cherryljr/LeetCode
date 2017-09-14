a4e42dfc2f61d6f5059762afa06c89a8e756dc28
cherryljr <travelerljr@gmail.com>
2017-09-14 18:17:53 +0900

    Binary Search O(nlogn)

--- Find Peak Element.java
+++ Find Peak Element.java
@@ -18,28 +18,28 @@ For example, in array [1, 2, 3, 1], 3 is a peak element and your function should
 class Solution {
     public int findPeakElement(int[] nums) {
         if (nums == null || nums.length <= 1) {
-			return 0;
+		return 0;
+	}
+
+	int len = nums.length;
+	if (nums[0] > nums[1]) {
+		return 0;
+	}
+	if (nums[len - 1] > nums[len - 2]) {
+		return len - 1;
+	}
+
+	int start = 0;
+	int end = len - 1;
+	while (start + 1 < end) {
+		int mid = start + (end - start) / 2;
+		if (nums[mid] < nums[mid + 1]) {
+			start = mid;
+		} else {
+			end = mid;
 		}
-		
-		int len = nums.length;
-		if (nums[0] > nums[1]) {
-			return 0;
-		}
-		if (nums[len - 1] > nums[len - 2]) {
-			return len - 1;
-		}
-		
-		int start = 0;
-		int end = len - 1;
-		while (start + 1 < end) {
-			int mid = start + (end - start) / 2;
-			if (nums[mid] < nums[mid + 1]) {
-				start = mid;
-			} else {
-				end = mid;
-			}
-		}
-		
-		return (nums[start] > nums[start + 1]) ? start : end;
+	}
+
+	return (nums[start] > nums[start + 1]) ? start : end;
     }
 }

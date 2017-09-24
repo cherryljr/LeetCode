e87dd95494e7cf5ac021d6d0b40f45619b4c54a8
cherryljr <travelerljr@gmail.com>
2017-09-25 02:00:57 +0900

    BFS

---
+++ Average of Levels in Binary Tree.java
@@ -0,0 +1,60 @@
+Basic question about Level order traversal using BFS.
+You can see the BFS Template here:
+https://github.com/cherryljr/LintCode/blob/master/BFS%20Template.java
+
+/*
+Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+
+Example 1:
+Input:
+    3
+   / \
+  9  20
+    /  \
+   15   7
+Output: [3, 14.5, 11]
+
+Explanation:
+The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
+
+Note:
+The range of node's value is in the range of 32-bit signed integer.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> rst = new ArrayList<>();
+        if (root == null)  {
+            return rst;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            rst.add(sum / size);
+        }
+        
+        return rst;
+    }
+}

905f3ac7598642569d84d0e4120dbc51612d65e5
cherryljr <travelerljr@gmail.com>
2017-09-29 00:14:59 +0900

    Same as Kth Smallest Number in Sorted Matrix in LintCode

--- Kth Smallest Number in Multiplication Table.java
+++ Kth Smallest Number in Multiplication Table.java
@@ -38,22 +38,26 @@ The k will be in the range [1, m * n]
 
 class Solution {
     public int findKthNumber(int m, int n, int k) {
-    	int low = 1 , high = m * n + 1;
+        int start = 1;
+        int end = m * n + 1;
         
-    	while (low < high) {
-    	    int mid = low + (high - low) / 2;
-    	    int c = count(mid, m, n);
-    	    if (c >= k) high = mid;
-            else low = mid + 1;
-    	}
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            int c = count(mid, m, n);
+            if (c < k) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
         
-    	return high;
+        return start;
     }
     
-    private int count(int v, int m, int n) {
+    public int count(int x, int m, int n) {
         int count = 0;
         for (int i = 1; i <= m; i++) {
-            int temp = Math.min(v / i , n);
+            int temp = Math.min(x / i, n);
             count += temp;
         }
         return count;

ef6af459ec5e3caf0925e7be6aabe07d61ab84ee
cherryljr <travelerljr@gmail.com>
2017-11-28 15:12:49 +0900

    Using Symmetric Transformation implements Rotation

---
+++ Rotate Image.java
@@ -0,0 +1,112 @@
+Using symmetic transformation implement matrix's rotate transformation
+    
+/*
+You are given an n x n 2D matrix representing an image.
+Rotate the image by 90 degrees (clockwise).
+
+Note:
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. 
+DO NOT allocate another 2D matrix and do the rotation.
+
+Example 1:
+
+Given input matrix = 
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+
+rotate the input matrix in-place such that it becomes:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+
+Example 2:
+
+Given input matrix =
+[
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
+], 
+
+rotate the input matrix in-place such that it becomes:
+[
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]
+]
+*/
+
+/*
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry by diagonal line
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+*/
+class Solution {
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return;
+        }
+        
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        // Reverse top to down
+        for (int i = 0; i < rows / 2; i++) {
+            for (int j = 0; j < cols; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[rows-i-1][j];
+                matrix[rows-i-1][j] = temp;
+            }
+        }
+        // Swap the symmetry
+        for (int i = 0; i < rows; i++) {
+            for (int j = i; j < cols; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
+
+/*
+ * anticlockwise rotate
+ * first reverse left to right, then swap the symmetry
+ * 1 2 3     3 2 1     3 6 9
+ * 4 5 6  => 6 5 4  => 2 5 8
+ * 7 8 9     9 8 7     1 4 7
+*/
+class Solution {
+    public void anti_rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return;
+        }
+        
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        // Reverse left to right
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][cols-j-1];
+                matrix[i][cols-j-1] = temp;
+            }
+        }
+        // Swap the symmetry by diagonal line
+        for (int i = 0; i < rows; i++) {
+            for (int j = i; j < cols; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}

4a09bc26aa55f145cb9622e239b8fa7c376040ed
cherryljr <travelerljr@gmail.com>
2017-12-11 17:59:44 +0900

    DFS (same as Number of Islands)

---
+++ Flood Fill.java
@@ -0,0 +1,62 @@
+/*
+An image is represented by a 2-D array of integers, each integer representing the pixel value of the image (from 0 to 65535).
+Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill, and a pixel value newColor, "flood fill" the image.
+To perform a "flood fill", consider the starting pixel, 
+plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, 
+plus any pixels connected 4-directionally to those pixels (also with the same color as the starting pixel), and so on. 
+Replace the color of all of the aforementioned pixels with the newColor.
+At the end, return the modified image.
+
+Example 1:
+Input: 
+image = [[1,1,1],[1,1,0],[1,0,1]]
+sr = 1, sc = 1, newColor = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: 
+From the center of the image (with position (sr, sc) = (1, 1)), all pixels connected 
+by a path of the same color as the starting pixel are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected
+to the starting pixel.
+
+Note:
+The length of image and image[0] will be in the range [1, 50].
+The given starting pixel will satisfy 0 <= sr < image.length and 0 <= sc < image[0].length.
+The value of each color in image[i][j] and newColor will be an integer in [0, 65535].
+*/
+
+/**
+ * DFS 
+ * Algorithm
+ * Say color is the color of the starting pixel. 
+ * Let's floodfill the starting pixel: we change the color of that pixel to the new color, 
+ * then check the 4 neighboring pixels to make sure they are valid pixels of the same color, 
+ * and of the valid ones, we floodfill those, and so on.
+ * We can use a function dfs to perform a floodfill on a target pixel.
+ * 
+ * Complexity Analysis
+ * Time Complexity: O(N), where N is the number of pixels in the image. We might process every pixel.
+ * Space Complexity: O(N), the size of the implicit call stack when calling dfs.
+ * 
+ * Note: this question is the same as: Number of Islands
+ * https://github.com/cherryljr/LeetCode/blob/master/Number%20of%20Islands.java
+ */
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int color = image[sr][sc];
+        if (color != newColor) {
+            dfs(image, sr, sc, color, newColor);
+        }
+        return image;
+    }
+    
+    public void dfs(int[][] image, int sr, int sc, int color, int newColor) {
+        if (sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != color) {
+            return;
+        }
+        image[sr][sc] = newColor;
+        dfs(image, sr + 1, sc, color, newColor);
+        dfs(image, sr - 1, sc, color, newColor);
+        dfs(image, sr, sc + 1, color, newColor);
+        dfs(image, sr, sc - 1, color, newColor);
+    }
+}

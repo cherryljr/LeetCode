7f0aa415952470073fb61d8a1cdbc350c6a7a7a8
cherryljr <travelerljr@gmail.com>
2017-12-31 21:00:02 +0900

    Big Integer Multiplication (Non-negative Numbers)

--- Multiply Strings.java
+++ Multiply Strings.java
@@ -29,7 +29,8 @@ class Solution {
             for (int j = num2.length() - 1; j >= 0; j--) {
                 int p1 = i + j, p2 = i + j + 1;
                 int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-                // The sum should plus the carry bit
+                // The current sum equals mul plus the ten's digit from the last mul 
+                // (pos[p2], or you can find from the draft directly, they are in the same column)
                 int sum = mul + pos[p2];
 
                 pos[p1] += sum / 10;

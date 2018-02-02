e0a31ddf060ae57639f9948119eb6397a2fcb0b3
cherryljr <travelerljr@gmail.com>
2018-02-03 02:33:10 +0900

    Interval DP (Similar to Strange Printer)

--- Burst Balloons.java
+++ Burst Balloons.java
@@ -1,25 +1,3 @@
-This Problem is very similar to Strange Printer in LeetCode.
-
-The nature way to divide the problem is burst one balloon and separate the balloons into 2 sub sections 
-one on the left and one one the right. 
-However, in this problem the left and right become adjacent and have effects on the maxCoins in the future.
-
-Then another interesting idea come up. Which is quite often seen in dp problem analysis. 
-That is reverse thinking. Like I said the coins you get for a balloon does not depend on the balloons already burst. 
-Therefore instead of divide the problem by the first balloon to burst, we divide the problem by the last balloon to burst.
-
-Why is that? Because only the first and last balloons we are sure of their adjacent balloons before hand!
-For the first we have nums[i-1]*nums[i]*nums[i+1] for the last we have nums[-1]*nums[i]*nums[n].
-OK. Think about n balloons if i is the last one to burst, what now?
-We can see that the balloons is again separated into 2 sections. 
-But this time since the balloon i is the last balloon of all to burst, 
-the left and right section now has well defined boundary and do not affect each other! 
-Therefore we can do either recursive method with memoization or dp.
-
-Here comes the final solutions. Note that we put 2 balloons with 1 as boundaries 
-and also burst all the zero balloons in the first round since they won't give any coins.
-The algorithm runs in O(n^3) which can be easily seen from the 3 loops in dp solution.
-
 /*
 Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented by array nums. 
 You are asked to burst all the balloons. If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins. 
@@ -37,36 +15,81 @@ Return 167
    coins =  3*1*5      +  3*5*8    +  1*3*8      + 1*8*1   = 167
 */
 
+/**
+ * Approach: Sequence DP
+ * 这道题目与 Strange Printer 有一定的类似，但是并不明显。
+ * 下面我将会分析开题的解题思路并与 Strange Printer 进行对比，希望能帮助大家理解。
+ * Strange Printer:
+ * https://github.com/cherryljr/LeetCode/blob/master/Strange%20Printer.java
+ *
+ * 思路：
+ * 题目想要求我们通过一种方式引爆气球使得最后的得分最高。
+ * 本题的难点在于：每次引爆一个气球之后，气球的 左边部分 和 右边部分 将会连接起来，
+ * 并且会对之后计算 maxCoins 产生影响。
+ * 这使得我们很难通过 分治 或者 动态规划 的方法去解决这个问题，因为我们无法很好地确认状态转移方程。
+ * 那么为了解决该问题，我们需要注意到非常重要的一点：
+ *  已经爆炸的气球不会再对 maxCoins 的计算产生任何影响！
+ * 这点非常重要！它提示我们可以 逆过来 思考问题，即：
+ *
+ * 我们从最后一个爆炸的气球开始，逆序推导回去。
+ * 这样我们就可以利用这个气球作为分割点来将序列分成左右两个部分，然后一步步逆推回去了。
+ *  eg. 假设 i 为最后一个爆炸的气球，那么 nums[0...i] 和 nums[i...end] 必定在这之前就已经被引爆了。
+ *  也就是说：这左右两个部分的 气球引爆情况 对彼此来说是没有任何影响的，因此也就可以被分割成两个部分了。
+ * 然后我们只需要枚举 每个区间长度中 每一个气球 在最后被引爆的情况，就能够得出 maxCoins 了。
+ * 对此我们用到了 序列的动态规划：
+ * dp[i][j] 表示区间 [i...j] 所能得到的 maxCoins.
+ *
+ * 接下来的分析情况与 Strange Printer 有些许类似 （为了便于理解，我尽量保持二者代码的一致以便对比）
+ *  首先，我们需要遍历所有 区间长度l, 并计算出它们对于的 maxCoins；
+ *  然后，在各个区间中，我们可以利用 pivot 作为分割点对区间进行分割以便我们计算出 maxCoins；
+ *  注意，根据以上分析，这里的 pivot 代表的就是这段区间中 最后一个被引爆的气球。
+ *  我们需要对区间中的各个位置进行一遍枚举，求出最佳位置才能得到 这段区间的maxCoins。
+ *  （同样，对应于 Strange Printer 中的 pivot 代表的是可能与 s[start] 相等的字符，
+ *  如果相等，就意味着可以对 dp[start][end] 的大小进缩减，即减少打印次数，
+ *  所以我们也要对它的位置进行 枚举 来求出该段区间的最少打印次数）。
+ *  回到本题，coins 的计算由左右两个点的分乘以 pivot 点的分所得。因此可推断出如下公式:
+ *  dp[start][end] = nums[start] * nums[pivot] * nums[end] + pivot左部分的最大值 + pivot右部分的最大值。
+ *  因为在计算 左/右部分 的maxCoins时需要用到 pivot 的值，所以 左右部分maxCoins和 公式为 dp[start][pivot] + dp[pivot[end].
+ *
+ * 时间复杂度分析：
+ *  因为我们需要对所有的区间长度进行一次分析，所以时间复杂度为：O(n^2)
+ *  而对于各个区间，我们还需要枚举 pivot 的值来进行分割以求得该区间的 maxCoins。
+ *  因此处理一个区间的时间复杂度为：O(n)
+ *  故，整体时间复杂为：O(n^3)
+ *  空间复杂度分析:
+ *  动态规划需要一个二维数组，因此时间复杂度为：O(n^2)
+ */
 class Solution {
     public int maxCoins(int[] iNums) {
         if (iNums == null || iNums.length == 0) {
             return 0;
         }
-        
+
+        // 因为当引爆最 左/右 边的气球时，它的一边值为 1.为了便于计算
+        // 我们新建一个 nums[], 其大小为 iNums.length+2, 最左/右边的一个元素为 1
         int[] nums = new int[iNums.length + 2];
-        int n = 1;
+        int len = 1;
         for (int x : iNums) {
             if (x > 0) {
-                nums[n++] = x;
+                nums[len++] = x;
             }
         }
-        nums[0] = nums[n++] = 1;
-        int[][] dp = new int[n][n];
-        // this operation could be canceled 
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = 1;
-        }
-        
-        for (int l = 2; l <= n; l++) {
-            for (int left = 0; left + l <= n; left++) {
-                int right = left + l - 1;
-                for (int k = left + 1; k < right; k++) {
-                    dp[left][right] = Math.max(dp[left][right], 
-                                              nums[left] * nums[k] * nums[right] + dp[left][k] + dp[k][right]);
+        nums[0] = nums[len++] = 1;
+        int[][] dp = new int[len][len];
+
+        // 遍历所有区间长度
+        for (int l = 2; l <= len; l++) {
+            for (int start = 0; start + l <= len; start++) {
+                int end = start + l - 1;
+                // 遍历当前区间，枚举 分段点pivot 并利用它将当前区间分段为 nums[start, pivot] 和 nums[pivot+1, end]
+                // 以得到最佳位置使得 区间s[start, end]的得分最多 (dp[start][end]有最大值)
+                for (int pivot = start + 1; pivot < end; pivot++) {
+                    dp[start][end] = Math.max(dp[start][end],
+                            nums[start] * nums[pivot] * nums[end] + dp[start][pivot] + dp[pivot][end]);
                 }
             }
         }
-        
-        return dp[0][n - 1];
+
+        return dp[0][len - 1];
     }
 }

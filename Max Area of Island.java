e74abceaf3c23028127eadb44bf1e2280b53d233
cherryljr <travelerljr@gmail.com>
2018-02-11 12:45:04 +0900

    DFS (Similar to Number of Big Islands)

--- Max Area of Island.java
+++ Max Area of Island.java
@@ -1,10 +1,3 @@
-The idea is to count the area of each island using DFS. 
-During the dfs, we set the value of each point in the island to 2. 
-The time complexity is O(mn).
-
-This question is the same as Number of Islands almostly.
-You can get detail explianations here: https://github.com/cherryljr/NowCoder/blob/master/%E5%B2%9B%E5%B1%BF%E4%B8%AA%E6%95%B0.java
-
 /*
 Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) 
 You may assume all four edges of the grid are surrounded by water.
@@ -26,13 +19,23 @@ Example 2:
 Given the above grid, return 0.
 */
     
+/**
+ * Approach: DFS
+ * The idea is to count the area of each island using DFS.
+ * During the dfs, we set the value of each point in the island to 2.
+ * The time complexity is O(mn).
+ * 
+ * This question is the same as Number of Big Islands almost.
+ * You can get detail explanations here:
+ * https://github.com/cherryljr/LeetCode/blob/master/Number%20of%20Big%20Islands.java
+ */
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        if (grid == null || grid.length == 0 || 
-            grid[0] == null || grid[0].length == 0) {
+        if (grid == null || grid.length == 0 ||
+                grid[0] == null || grid[0].length == 0) {
             return 0;
         }
-        
+
         int rst = 0;
         int rows = grid.length;
         int cols = grid[0].length;
@@ -44,21 +47,24 @@ class Solution {
                 }
             }
         }
-        
+
         return rst;
     }
-    
+
     // DFS
     private int infect(int[][] grid, int i, int j, int rows, int cols, int area) {
         if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] != 1) {
             return area;
         }
-        grid[i][j] = 2;  // Mark the explored island cells with 2.
+
+        // Mark the explored island cells with 2.
+        grid[i][j] = 2;
         area++;
-        area = infect(grid, i + 1, j, rows, cols, area);
-        area = infect(grid, i - 1, j, rows, cols, area);
-        area = infect(grid, i, j + 1, rows, cols, area);
-        area = infect(grid, i, j - 1, rows, cols, area);
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for (int[] dir : dirs) {
+            area = infect(grid, i + dir[0], j + dir[1], rows, cols, area);
+        }
+
         return area;
     }
 }

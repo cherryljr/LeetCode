81a23c5bb10c256e71afa27b51da62611c0bd279
cherryljr <travelerljr@gmail.com>
2018-02-19 01:12:49 +0900

    Two Pointers

---
+++ Minimum Size Subarray Sum.java
@@ -0,0 +1,49 @@
+/*
+Given an array of n positive integers and a positive integer s,
+find the minimal length of a contiguous subarray of which the sum ≥ s.
+If there isn't one, return 0 instead.
+
+For example, given the array [2,3,1,2,4,3] and s = 7,
+the subarray [4,3] has the minimal length under the problem constraint.
+ */
+
+/**
+ * Approach: Two Pointers
+ * Initialize left pointer to 0 and sum to 0
+ * Iterate over the nums:
+ *  Add nums[right] to sum
+ *  While sum is greater than or equal to s:
+ *      1. Update rst=min(rst,right−left+1), where right−left+1 is the size of current subarray
+ *      2. It means that the first index can safely be incremented,
+ *      since, the minimum subarray starting with this index with sum≥s has been achieved
+ *      3. nums[left] from sum and increment left
+ *
+ * Complexity analysis
+ *  Time complexity: O(n). Single iteration of O(n).
+ *  Each element can be visited at most twice, once by the right pointer(i) and (at most)once by the left pointer.
+ *  Space complexity: O(1) extra space.
+ *  Only constant space required for left, right, sum and rst.
+ *
+ * Reference:
+ * https://leetcode.com/articles/minimum-size-subarray-sum/#
+ */
+class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int left = 0;
+        int sum = 0;
+        int rst = Integer.MAX_VALUE;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while (sum >= s) {
+                rst = Math.min(rst, right - left + 1);
+                sum -= nums[left++];
+            }
+        }
+
+        return rst == Integer.MAX_VALUE ? 0 : rst;
+    }
+}

af1da7cb92f506ab4d767d12bd271cb87fb78827
cherryljr <travelerljr@gmail.com>
2018-04-07 15:03:41 +0900

    Recursion | DP | Two Pointers

--- Wildcard Matching.java
+++ Wildcard Matching.java
@@ -71,6 +71,8 @@ class Solution {
  * 即该矩阵是 从下到上，从右到左 进行填充的，最终需要的状态为 dp[0][0]
  * 对此我们需要对 dp 矩阵进行一个初始化,使得我们可以根据初始化之后的 base case 推测出矩阵中的其他状态。
  * 这里需要初始化的状态为：最后一行以及最后一列
+ *
+ * 时间复杂度：O(mn)
  */
 class Solution {
     public boolean isMatch(String s, String p) {
@@ -104,5 +106,70 @@ class Solution {
         }
 
         return dp[0][0];
+    }
+}
+
+/**
+ * Approach 3: Two Pointers
+ * The basic idea is to have one pointer for the string and one pointer for the pattern.
+ * 
+ * We need the traverse the String and compare the sth character in String with the pth character in pattern.
+ * There are only four situation here:
+ *  1. str.charAt(s) == pattern.charAt(p)
+ *  In this situation, we need move pNode and sNode to the next position and continue the process.
+ *  2. str.charAt(s) != pattern.charAt(p) && pattern.charAt(p) == '?'.
+ *  Cuz '?' Matches any single character, the situation is the same as situation 1. sNode++ and pNode++ then continue the process.
+ *  3. str.charAt(s) != pattern.charAt(p) && pattern.charAt(p) == '*'.
+ *  Cuz '*' Matches any sequence of characters.
+ *  Here, we use starIndex to store the current position of pNode (the position of character '*')
+ *  and use match to store the current position of sNode (the starting position of String to match '*').
+ *  Then we only need to move the pNode to the next position (cuz '*' can alsoo matches the empty sequence, sNode needn't to move).
+ *  4. str.charAt(s) != pattern.charAt(p) && starIndex != -1.
+ *  It means that we have encountered a '*' in pattern and we stored the position of it.
+ *  If the character in String has matched the '*' already, the match will move to the next position (match++), then sNode = match.
+ *  This is very important, you can think about this case: String - "abcdabcd", Pattern - "a*bcd" then you will understand it.
+ *  Set pNode as the next position of '*' (pNode = starIndex + 1).
+ *  if the situation isn't in above, we will return false.
+ * After traverse the String, we also need to check for remaining characters in pattern.
+ * If the remaining character is '*', then p++.
+ * At the end, we only need to check whether the pNode has traverse to the end or not.
+ *
+ * Time complexity is O(p*s), where p and s are the lengths of the pattern and input strings. 
+ */
+class Solution {
+    public boolean isMatch(String str, String pattern) {
+        int s = 0, p = 0, match = 0, starIndex = -1;
+        while (s < str.length()) {
+            // advancing both pointers
+            if (p < pattern.length() && (pattern.charAt(p) == '?'
+                    || str.charAt(s) == pattern.charAt(p))) {
+                s++;
+                p++;
+            }
+            // '*' found, only advancing pattern pointer
+            else if (p < pattern.length() && pattern.charAt(p) == '*') {
+                starIndex = p;
+                match = s;
+                p++;
+            }
+            // we have store a '*' position in pattern string, advancing string pointer
+            else if (starIndex != -1) {
+                p = starIndex + 1;
+                match++;
+                s = match;
+            }
+            // current pattern pointer is not star, last patter pointer was not '*'
+            // characters do not match
+            else {
+                return false;
+            }
+        }
+
+        // check for remaining characters in pattern
+        while (p < pattern.length() && pattern.charAt(p) == '*') {
+            p++;
+        }
+
+        return p == pattern.length();
     }
 }

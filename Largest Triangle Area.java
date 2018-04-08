9df4e6294db607183a6ca556b346c3f840256d97
cherryljr <travelerljr@gmail.com>
2018-04-08 16:51:51 +0900

    Cross Product

--- Largest Triangle Area.java
+++ Largest Triangle Area.java
@@ -1,34 +1,56 @@
 /*
-有一堆共 M 个鹰蛋，一位教授想研究这些鹰蛋的坚硬度 E。
+You have a list of points in the plane.
+Return the area of the largest triangle that can be formed by any 3 of the points.
+
+Example:
+Input: points = [[0,0],[0,1],[1,0],[0,2],[2,0]]
+Output: 2
+Explanation:
+The five points are show in the figure below. The red triangle is the largest.
+Picture:
+https://s3-lc-upload.s3.amazonaws.com/uploads/2018/04/04/1027.png
+
+Notes:
+3 <= points.length <= 50.
+No points will be duplicated.
+ -50 <= points[i][j] <= 50.
+Answers within 10^-6 of the true value will be accepted as correct.
 */
 
+/**
+ * Approach: Using Cross Product
+ * 关于三角形的面积计算，有非常多的公式....
+ * 而在计算机计算中通常使用的就是 Area = 1/2 * |AB| * |AC| * sinA
+ * 而 |AB| * |AC| * sinA 就是向量 AB 与 AC 的叉积（外积）
+ * 本题提供了各个点的坐标，因此我可以计算出各个向量，然后再计算出 1/2*向量的外积 就是三角形的面积了。
+ * 这里用到了 行列式 的计算，不会的快去面壁...
+ * 枚举三角形三个点所有组合的方法，计算出最大面积。
+ * 时间复杂度为：O(n^3)
+ *
+ * 对于三角形面积计算这一点，知乎上有个专栏，大家有兴趣可以去看一看：
+ * https://zhuanlan.zhihu.com/p/25599977
+ *
+ * 向量外积的另一个应用：
+ * https://github.com/cherryljr/LeetCode/blob/master/Convex%20Polygon.java
+ */
 class Solution {
-    /**
-     * @param m 蛋的数目
-     * @param n 楼层的高度
-     * @return 最坏情况下的最小代价(扔多少次)
-     */
-    public int dropEggs(int m, int n) {
-        if (n == 0) {
-            return 0;
-        }
-        
-        int ans = 0;
-        int[][] dp = new int[m+1][n+1];
-        for (int i = 1; i <= n; ++i) {
-            dp[1][i] = i;
-        }
-
-        for(int i = 2; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                dp[i][j] = dp[i][j-1] + 1;
-                for(int k = 1; k < j; ++k) {
-                    dp[i][j] = Math.min(dp[i][j], 
-                                        Math.max(dp[i-1][k-1], dp[i][j-k]) + 1);
+    public double largestTriangleArea(int[][] points) {
+        double area = 0;
+        for (int[] A : points) {
+            for (int[] B : points) {
+                for (int[] C : points) {
+                    area = Math.max(area, 0.5 * calculateArea(A, B, C));
                 }
             }
         }
-        
-        return dp[m][n];
+        return area;
+    }
+
+    private double calculateArea(int[] A, int[] B, int[] C) {
+        int ABx = B[0] - A[0];
+        int ABy = B[1] - A[1];
+        int ACx = C[0] - A[0];
+        int ACy = C[1] - A[1];
+        return Math.abs(ABx * ACy - ABy * ACx);
     }
 }

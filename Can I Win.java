9282b9d8bad34a29e67cae50746bc20d9851e40b
cherryljr <travelerljr@gmail.com>
2018-05-25 20:15:42 +0900

    Game Theory (Using Recursion + Memory Search)

--- Can I Win.java
+++ Can I Win.java
@@ -26,85 +26,68 @@ Same with other integers chosen by the first player, the second player will alwa
 */
 
 /**
- * 这题似乎是一题经典的博弈问题，但是有一个很重要的限制条件 —— 每个数只能取一次。
- * 这也是该题解题的核心点所在。
- * 有了这个条件，我们很容易就能想到所有的取数方案，最多有 maxChoosableInteger! 种，即 1~maxChoosableInteger 的全排列。
- * 但是一一枚举这 maxChoosableInteger! 种方案明显超过了能承受的时间复杂度。
- * 因为方案有很多的部分重复计算了，所以我们可以采用记忆化搜索的方法来优化该算法。
+ * Approach: Game Theory (Using Recursion + Memory Search)
+ * 这道题目属于博弈论的类型，其实质是一个求 Permutations 的过程。
+ * 在取数的过程中，安装取的顺序，我们总共有 maxChoosableInteger! 种方案，即 1~maxChoosableInteger 的全排列。
+ * 但是枚举 maxChoosableInteger! 种方案明显超过了能承受的时间复杂度。
+ * 原因在于我们进行了大量重复的不必要计算。
  *
- * 在记忆化搜索中，使用了两个条件判断胜利：
- * 一是当前剩下的desiredTotal(减去了取走的数)小于等于0，
- * 二是对于剩下的还未取的数，已经搜索过且是必胜的状态，
- * 假如这个状态没有搜索过，就进行 dfs 判断这个状态，直到遇到判断过的状态或desiredTotal小于等于0。
+ * 分析题目，我们发现，只要当前状态已经确定，那么结果也就确定了。
+ * 即只要游戏还没结束，我们并不关心如何来到当前状态。如：玩家取了 1,3 和 3,1 结果是相同的。
+ * 因此我们可以采用记忆化搜索的方法将已经计算过的状态保存下来，从而达到优化时间复杂度的目的。
  *
- * 对于记录状态的数组，
- * 我们使用 01 表示，0代表这个数没有使用过，1代表已经使用过。
- * 把 1~maxChoosableInteger 的使用情况定义为一个只有0,1的向量，
- * 比如：maxChoosableInteger=3，他们的使用情况有8种，
- * 分别是000(没有元素被使用),001,010,100,011,110,101,111(所有元素都被使用)。
- * 再把这个向量转化成十进制数作为下标。
+ * 递归过程中，我们关心的状态有：当前所能取到的最大值（这个是不变的）；当前所取到值之和；以及那些数已经被取过了
+ * 这里我们稍微进行了 两点 空间复杂度上的优化：
+ *  1. 使用 dp[] 来记录计算出来的结果。因为其只有 3 中状态，所以我们使用 byte 就够了，开 int[] 浪费了。
+ *  2. 使用一个 int 来记录哪些数被取过。因为 maxChoosableInteger 不会超过 20，而 int 有 32 位，完全够用了。
+ *  这个做法非常常见，包括大家所熟悉的 BitMap 也使用到了这个做法。
  *
- * 复杂度分析：
- * 除了特殊情况外我们需要计算所有的状态，且每个状态只需要计算一次，则时间和空间复杂度均为O(2^n)。
- * n 为 maxChoosableInteger. （如果不使用记忆化搜索，时间复杂度将是：n!）
- * 
- * 参考：https://leetcode.com/problems/can-i-win/discuss/
+ * 时间复杂度：O(2^n)
+ *
+ * 参考资料：https://www.youtube.com/watch?v=GNZIAbf0gT0
  */
 class Solution {
-    int[] dp;
-    boolean[] used;
+    byte[] dp;  // 0: unknow, 1: can win, -1: can't win
+
     public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
-        int sum = (1 + maxChoosableInteger) * maxChoosableInteger / 2;
-        // 取完所有数，也达不到desiredTotal，无法赢得游戏
-        if (sum < desiredTotal) {
+        int sum = maxChoosableInteger * (maxChoosableInteger + 1) >> 1;
+        // 取出所有的数都凑不出 desiredTotal，肯定输
+        if (desiredTotal > sum) {
             return false;
         }
-        // 第一步就可以获得胜利
+        // 可取的最大值直接 >= 期望值，先手必赢
         if (desiredTotal <= maxChoosableInteger) {
             return true;
         }
 
-        dp = new int[1 << maxChoosableInteger];
-        Arrays.fill(dp , -1);
-        used = new boolean[maxChoosableInteger + 1];
-
-        return helper(desiredTotal);
+        // dp[]的 index 就表示了其中一种方案.（思想与利用 二进制 计算subset相同）
+        dp = new byte[1 << maxChoosableInteger];
+        return canIWinHelper(maxChoosableInteger, desiredTotal, 0);
     }
 
-    public boolean helper(int desiredTotal) {
+    private boolean canIWinHelper(int maxChoosableInteger, int desiredTotal, int state) {
+        // if the previous player make desiredTotal <= 0, it means he can win, then the current player lose.
         if (desiredTotal <= 0) {
             return false;
         }
-        // 把used数组转为十进制表示
-        int key = format(used);
-        if (dp[key] == -1) {
-            // 枚举未选择的数
-            for (int i = 1; i < used.length; i++) {
-                if (!used[i]) {
-                    used[i] = true;
-                    // 检查选择该数能否赢得游戏 （即让另一个玩家输掉游戏）
-                    if (!helper(desiredTotal - i)) {
-                        dp[key] = 1;
-                        used[i] = false;
-                        return true;
-                    }
-                    used[i] = false;
-                }
-            }
-            dp[key] = 0;
+        // if the current state has been calculated before, return the result directly
+        if (dp[state] != 0) {
+            return dp[state] == 1;
         }
-        return dp[key] == 1;
-    }
 
-
-    public int format(boolean[] used) {
-        int num = 0;
-        for (boolean b: used) {
-            num <<= 1;
-            if(b) {
-                num |= 1;
+        for (int i = 0; i < maxChoosableInteger; i++) {
+            // if number i is used, skip it
+            if (((state >> i) & 1) > 0) {
+                continue;
+            }
+            // the next player can't win, them the current player can win
+            if (!canIWinHelper(maxChoosableInteger, desiredTotal - (i + 1), state | (1 << i))) {
+                dp[state] = 1;
+                return true;
             }
         }
-        return num;
+
+        dp[state] = -1;
+        return false;
     }
 }

5b6c243b500f25a88271b1dbed29f0783b423924
cherryljr <travelerljr@gmail.com>
2019-03-23 22:40:33 +0800

    Mathematics (Count Numbers with Detail Explantion)

--- Numbers At Most N Given Digit Set.java
+++ Numbers At Most N Given Digit Set.java
@@ -27,53 +27,108 @@ D is a subset of digits '1'-'9' in sorted order.
  */
 
 /**
- * Approach: Mathematics
+ * Approach 1: Mathematics (Count Numbers)
  * 根据数据规模可得，时间复杂度应该在 logn 级别。
  * 因此我们应该根据位数对可能性进行分析，而不是一个个数进行遍历的 Brute Force.
- * 根据数学分析，我们从高位开始看，
+ * 该题属于 计数类 问题。根据数学分析，我们从高位开始看，
  * 首先我们可以加上出去最高位的所有可能性。因为这些数字必定是小于 N 的。
  * eg. N = 52525, 那么我们可以加上所有的：
- *  X, XX, XXX, XXXX 这些数，每个位上数的可能性均为 D.length.
- *  因此综合为 sum(D.length^i) 1<=i<=len-1
+ *  X, XX, XXX, XXXX 这些数，每个位上数的可能性均为 D.length. 因此 ans += pow(D.length, i)
  * 然后我们再从最高位开始分析：
  *  如果该位上的数 > D[i]，那么就意味着可以组成以 D[i] 为高位的，总共有 D.length^(n-i-1) 的个数比 N 要小。
- *  依次类推下去；
- *  如果该位上的数 = D[i]，那么无法确定，我们需要进行向后判断，即判断较低位上的数。其实就是一个子问题罢了。
+ *  如果该位上的数 = D[i]，那么无法确定，我们需要进行向后判断，即判断较低位上的数。其实就是一个子问题罢了，继续求解下一位状态即可。
+ *  如果该位上的数 < D[i]，那么说明没有继续组成 <=N 的元素了（因为 set 是排序好的，后面的 D[j] 只会比 D[i] 更大），
+ *  此时直接 return ans 即可。
+ *  即:核心就是，check当前位置上的数是否存在于Set D[] 之中，如果存在的话，就会导致当前结果无法确定，需要继续遍历下一位
+ *  否则的话，就能确定当前结果 ans += sum(d[i] < s[i]) * pow(D.length, n - i - 1)
  *
  * 时间复杂度：O(log10(N))
  * 空间复杂度：O(1)
  *
  * 参考资料：
  *  https://www.youtube.com/watch?v=d2O_jwPxroc
+ * 类似的问题：
+ * Numbers With Repeated Digits:
+ *  https://github.com/cherryljr/LeetCode/blob/master/Numbers%20With%20Repeated%20Digits.java
+ * Rotated Digits:
+ *  https://github.com/cherryljr/LeetCode/blob/master/Rotated%20Digits.java
  */
 class Solution {
     public int atMostNGivenDigitSet(String[] D, int N) {
-        char[] s = String.valueOf(N).toCharArray();
-        int len = s.length;
-        int rst = 0;
+        // 注意：按照上述解法分析，如果N中的每一位数均在 set D[] 中的话，那么循环将会一直遍历到 N 的最后一位。
+        // 因为我们只对 <s[i] 的元素进行 add pow(D.length, n-i-1)操作，当最后一位仍然出现 D.contains(s[i]) 的情况。
+        // 程序只能继续去遍历N的下一位，但是下一位已经没有了。
+        // 因此，此时如果直接 return 的话，结果就会少一个数（就是N本身）
+        // 即算法实际上计算的是 <N 的结果，故这里对 N 进行了一次 +1 操作。
+        // 因为题目求的是 整数，所以 ans(<=N) == ans(<N+1)
+        char[] s = String.valueOf(N + 1).toCharArray();
+        int n = s.length, ans = 0;
         // 加上所有小于 ?XXX... 的数字个数。即不考虑最高位。
-        for (int i = 1; i < len; i++) {
-            rst += (int)Math.pow(D.length, i);
+        for (int i = 1; i < n; i++) {
+            ans += (int)Math.pow(D.length, i);
         }
 
-        for (int i = 0; i < len; i++) {
-            boolean prefix = false;
+        for (int i = 0; i < n; i++) {
+            boolean prefixMatched = false;
             for (String num : D) {
                 if (s[i] > num.charAt(0)) {
-                    rst += (int)Math.pow(D.length, len - i  - 1);
+                    ans += (int)Math.pow(D.length, n - i - 1);
                 } else if (s[i] == num.charAt(0)) {
-                    // check the next digit
-                    prefix = true;
+                    // 如果当前位置上的数与num相等（存在于set之中），那么我们无法判断，只能去 check 下一位数的大小（子问题）
+                    prefixMatched = true;
+                    break;
+                } else {
+                    // 如果当前位置上的数小于num，那么直接break，因为set中后面的元素必定与当前元素要大
                     break;
                 }
             }
 
-            if (!prefix) {
-                return rst;
+            if (!prefixMatched) {
+                // 能运行到这说明当前位置上的数不存在于 set 之中，此时已经可以确定所有 <N 的元素个数已经统计完毕
+                break;
             }
         }
 
-        // 如果运行到这说明 N 正好可以被 D 中的数字组合而成，因此需要 +1
-        return rst + 1;
+        return ans;
+    }
+}
+
+/**
+ * Approach 2: Mathematics (More Concise)
+ * Approach 1 中对各个情况进行了详细的分析，但是对于 s[i] 和 D[j] 值大小的比较上（三种情况）处理并不够优雅。
+ * 分析的时候，但是对于这三种情况，其实 s[i] < D[i] 和 s[i] > D[i] 在 for 循环的处理上是相同的，
+ * 只有 s[i] == D[i] 时，才无法确定结果，需要继续遍历下一位上的值。
+ * 因此根据这点，我们可以对代码进行一定的简化。
+ *
+ * 时间复杂度：O(lgN * D.length)
+ * 空间复杂度：O(D.length)
+ *
+ * PS.我是真的不想吐槽...D里面的元素都是"1", "2", "3"...这种一个数的String...就不能给个 char[] 吗...
+ */
+class Solution {
+    public int atMostNGivenDigitSet(String[] D, int N) {
+        char[] s = String.valueOf(N + 1).toCharArray();
+        int n = s.length, ans = 0;
+        for (int i = 1; i < n; i++) {
+            // D中所有的元素范围为 1~9，所以不需要担心最高位出现 0 的情况
+            ans += (int)Math.pow(D.length, i);
+        }
+
+        Set<Character> set = new HashSet<>();
+        for (String num : D) {
+            set.add(num.charAt(0));
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < D.length && D[j].charAt(0) < s[i]; j++) {
+                ans += Math.pow(D.length, n - i - 1);
+            }
+            // 如果 s[i] 不在 set D[] 里面，那么状态已经可以确定，
+            // 说明所有 <N 的元素个数已经统计完毕, 直接退出循环即可
+            if (!set.contains(s[i])) {
+                break;
+            }
+        }
+
+        return ans;
     }
 }

be31fddb23bce98b7eee9b958a2fbe111f512b60
cherryljr <TravelerLjr@gmail.com>
2019-04-11 00:22:27 +0800

    Swap and Reverse

--- Next Permutation.java
+++ Next Permutation.java
@@ -27,7 +27,7 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
  
  
 /**
- * Approach 2: Single Pass Approach
+ * Approach 2: Swap and Reverse
  * Algorithm
  * We need to find the first pair of two successive numbers a[i] and a[i−1], from the right, which satisfy a[i] > a[i-1]. 
  * Now, no rearrangements to the right of a[i−1] can create a larger permutation since that subarray consists of numbers in descending order. 
@@ -35,10 +35,10 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
  * 
  * Now, what kind of rearrangement will produce the next larger number? 
  * We want to create the permutation just larger than the current one. 
- * Therefore, we need to replace the number a[i−1] with the number which is just larger than itself 
- * among the numbers lying to its right section, say a[j].
+ * Therefore, we need to swap the number a[i−1] and the number a[larger] which is just larger than itself 
+ * among the numbers lying to its right section, say a[larger].
  * 
- * We swap the numbers a[i−1] and a[j]. 
+ * We swap the numbers a[i−1] and a[larger]. 
  * We now have the correct number at index i−1. 
  * But still the current permutation isn't the permutation that we are looking for. 
  * We need the smallest permutation that can be formed by using the numbers only to the right of a[i−1]. 
@@ -46,7 +46,7 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
  * But, recall that while scanning the numbers from the right, 
  * we simply kept decrementing the index until we found the pair a[i] and a[i−1] where, a[i] > a[i-1]. 
  * Thus, all numbers to the right of a[i−1] were already sorted in descending order. 
- * Furthermore, swapping a[i−1] and a[j] didn't change that order. 
+ * Furthermore, swapping a[i−1] and a[larger] didn't change that order. 
  * Therefore, we simply need to reverse the numbers following a[i−1] to get the next smallest lexicographic permutation.
  * 
  * Complexity Analysis
@@ -62,7 +62,7 @@ class Solution {
             index--;
         }
         
-        // replace the number nums[index] with the number a[larger] which is just larger than itself 
+        // swap the number nums[index] with the number a[larger] which is just larger than itself 
         if (index >= 0) {
             int larger = nums.length - 1;
             while (larger >= 0 && nums[larger] <= nums[index]) {
